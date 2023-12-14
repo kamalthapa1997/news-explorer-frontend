1328e4a6d958a869d271eec6df45baad2a8fba6a
Kamal Thapa <handsomelotus1@cmail.com>
2023-12-14 12:19:18 -0500

    feat:  deploying the site to the server

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "production"
     ? "https://api.newsofworld.twilightparadox.com"
     : "http://localhost:3000";
 
